794baa1614bb7159daf51ec6ac48f55918c58aa3
AttaUrRehman <atta.ur.rehman@codility.co>
2019-03-04 14:48:47 +0500

    resolve some conflicts

--- resources/views/mail_checkin.blade.php
+++ resources/views/mail_checkin.blade.php
@@ -4,7 +4,22 @@
 @component('mail::panel')
 Following employees are not marked CHECK IN or not present in office Yet .
 @endcomponent
+
 @if(isset($get_users_collection))
+    @foreach($get_users_collection as $user)
+        @php
+            $inform=$user->informs()->whereBetween('attendance_date', [\Carbon\Carbon::now()->startOfDay()->timestamp, \Carbon\Carbon::now()->timestamp])->first();
+        @endphp
+        @if($inform->inform_type == "LEAVE")
+            @php $informleave=true;@endphp
+        @endif
+        @if($inform->inform_type == "LATE")
+            @php $informlate=true;@endphp
+        @endif
+    @endforeach
+@endif
+@if(isset($get_users_collection))
+    @if(isset($informleave))
  The following list having those employees who have Informed Leave
 @component('mail::table')
     |  Employee  Name     | Informed At                                                                                                                                                                                                                                     |        Informed Type                                                                            |   Inform status                               | Informed Reason              |
@@ -16,19 +31,19 @@ Following employees are not marked CHECK IN or not present in office Yet .
         @if($inform->inform_type == "LEAVE")
         |     {{$user->name}}       | {{$inform?\Carbon\Carbon::parse($inform->inform_at)->format('M ,d g:i A'):''}}|{{$inform?($inform->request_id != null ? ($inform->get_request_leave->approved):''):''}}{{$inform?$inform->inform_type:''}}  {{$inform?($inform->leaves?' ON '.$inform->leaves->name:''):''}} | {{$inform?($inform->inform_late?'Yes Late Informed':'No Late Informed'):''}} | @php $reason=str_replace("<br />", "\n",$inform?$inform->reason:''); echo $reason; @endphp |
         @endif
-        @if($inform->inform_type == "LATE")
-        @php $informlate=true;@endphp
-        @endif
+
     @endforeach
 
     
 @endcomponent
 
 @endif
+@endif
 
 @if(isset($get_users_collection))
-    @if(isset($informlate))
- The following list having those employees who have Informed Late
+@if(isset($informlate))
+
+The following list having those employees who have Informed Late
 @component('mail::table')
         |  Employee  Name     | Informed At                                                                                                                                                                                                                                     |        Informed Type                                                                            |   Inform status                               | Informed Reason              |
         | ------------------- | ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- | ----------------------------------------------------------------------------------------------- | --------------------------------------------- | ---------------------------- |
