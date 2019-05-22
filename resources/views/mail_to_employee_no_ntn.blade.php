1f75e44cb4608d4068c8ad0c2d0a081e3e3d3ae5
AttaUrRehman <atta.ur.rehman@codility.co>
2019-05-22 14:27:02 +0500

    Add CC and change Phone number in Employee NTN notification Email according to Hussnaibn

--- resources/views/mail_to_employee_no_ntn.blade.php
+++ resources/views/mail_to_employee_no_ntn.blade.php
@@ -8,8 +8,8 @@ Thank you !
 <br>
 <br>
 From HR Department, {{ config('app.name') }}<br>
-Mobile   : 0307-6823026,03064188742<br>
-LandLine : 0423-8937152, 0423-8910394 <br>
+Mobile   : 0304-4145724<br>
+LandLine : 0423-5222747 <br>
 
 <p style="float: right;">Email Created On : {{Carbon\Carbon::now()->toDayDateTimeString()}} </p><br>
 @endcomponent
